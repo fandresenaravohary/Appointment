e6a28d3d48d1ea83935307a5f69a3c8e31afba61
Fandresena Ravohary <hei.fandresena@gmail.com>
2023-08-30 20:35:15 +0100

    Feat: add the connection to the database

---
+++ connection/DatabaseConnection.java
@@ -0,0 +1,21 @@
+package com.appointment_management.connection;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+public class DatabaseConnection {
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                System.getenv("DB_URL"),
+                System.getenv("DB_USER"),
+                System.getenv("DB_PASSWORD")
+        );
+    }
+}
